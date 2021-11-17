1cb2405327b86854157196951e811e1aab4b9fb1
Talhah Awan <talhahawan@gmail.com>
2021-11-16 22:26:51 -0600

    added model relations

--- models/index.js
+++ models/index.js
@@ -2,3 +2,37 @@ const Trainer = require('./Trainer');
 const Item = require('./Item');
 const Pokemon = require('./Pokemon');
 const Order = require('./Order');
+
+trainer.hasMany(pokemon, {
+    foreignKey: 'trainer_id',
+});
+
+trainer.hasMany(item, {
+    foreignKey: 'trainer_id',
+});
+
+trainer.hasMany(order, {
+    foreignKey: 'trainer_id',
+});
+
+pokemon.hasOne(trainer, {
+    foreignKey: 'pokemon_id',
+});
+
+item.hasOne(trainer, {
+    foreignKey: 'item_id',
+});
+
+order.hasOne(trainer, {
+    foreignKey: 'trainer_id',
+});
+
+order.hasMany(pokemon, {
+    foreignKey: 'pokemon_id',
+});
+
+order.hasMany(item, {
+    foreignKey: 'item_id',
+});
+
+module.exports = { trainer, pokemon, item, order };
