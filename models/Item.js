cbc71b1efdb61a099a3a0a37f36e2267d7def729
Bonnie Reed <bonniejeanreed@gmail.com>
2021-11-19 00:05:56 -0600

    updated handlebar data with new seed info

--- models/Item.js
+++ models/Item.js
@@ -4,44 +4,44 @@ const sequelize = require('../config/connection');
 class Item extends Model {}
 
 Item.init(
-    {
-        item_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        type: {
-            type: DataTypes.STRING,
-        },
-        price: {
-            type: DataTypes.DECIMAL,
-            allowNull: false,
-        },
-        trainer_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'trainer',
-                key: 'trainer_id',
-            },
-            allowNull: true,
-        },
-        sprites: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
+  {
+    item_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'item',
-    }
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    types: {
+      type: DataTypes.STRING,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    trainer_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'trainer',
+        key: 'trainer_id',
+      },
+      allowNull: true,
+    },
+    sprites: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'item',
+  }
 );
 
 module.exports = Item;
