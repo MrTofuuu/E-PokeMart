e25fce703b9741148089bd2ce7eacf67bfff6a07
Bonnie Reed <bonniejeanreed@gmail.com>
2021-11-19 23:16:29 -0600

    Made changes to cards to include ids to be added to checkout array

---
+++ public/js/checkout.js
@@ -0,0 +1,11 @@
+var add = document.getElementById('addToCart');
+var del = document.getElementById('delete');
+var checkout = document.getElementById('checkout');
+
+for (var i = 0; i < addItem.length; i++) {
+  console.log(addItem[i]);
+}
+
+add.addEventListener('click');
+del.addEventListener('click');
+checkout.addEventListener('click');
