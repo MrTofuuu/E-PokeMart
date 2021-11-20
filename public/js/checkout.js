76b8d8d418eb24b26af7f857d80c0517f43ecfae
Bonnie Reed <bonniejeanreed@gmail.com>
2021-11-20 01:42:45 -0600

    added cart array from item ids

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -1,11 +1,39 @@
-var add = document.getElementById('addToCart');
-var del = document.getElementById('delete');
-var checkout = document.getElementById('checkout');
+// const del = document.querySelectorAll('delete');
+// const addBtn = document.querySelectorAll('addToCart');
+// const checkout = document.querySelectorAll('checkout');
+// const button = document.querySelectorAll('btn');
+// let id = button.getAttribute('data-id');
 
-for (var i = 0; i < addItem.length; i++) {
-  console.log(addItem[i]);
+// for (var i = 0; i < addItem.length; i++) {
+//   console.log(addItem[i]);
+// }
+
+let cart = [];
+let trainerArr = [];
+
+function addToCart(clicked_id) {
+  clicked_id = parseInt(clicked_id);
+  cart.push(clicked_id);
+  console.log(cart);
 }
 
-add.addEventListener('click');
-del.addEventListener('click');
-checkout.addEventListener('click');
+// function addCartArr() {
+//   checkoutArr.push();
+// }
+
+// del.innerHTML = deleteArr;
+
+// function deleteArr() {
+//   checkoutArr.splice();
+// }
+
+// checkout.innerHTML = checkoutArr;
+
+// function checkoutArr() {
+//   trainerArr.push();
+// }
+
+// button.addEventListener('click', function (event) {
+//   event.preventDefault();
+//   cart.push(id);
+// });
